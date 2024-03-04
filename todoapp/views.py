78e5bc25da3caa46ccc1506c49e8fe2a654eefea
Jochen Mößner <jochen.moessner@teilnehmer-bfwsb.de>
2024-03-04 16:24:10 +0100

    2024-03-04 16:24

--- todoapp/views.py
+++ todoapp/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render
 from .models import *
 
 # Create your views here.
-
+  
 def home(request):
-    liste = Eintrag.objects.filter(done = False)
+    liste = Eintrag.objects.filter(done=False)
+    user = "Moerli"
     content = {
-        'liste':liste
+        'liste': liste,
+        'user': user,
     }
     return render(request, "todoapp/todo_list.html", content)
